24e8a45a96d71ed1429517a19f5a01d2f40e405d
Pedro Canafistula <peddrocamposcanafistla@gmail.com>
2025-03-03 14:31:56 -0300

    Atividade 1.01

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,13 @@
+let num1 = 5;
+let num2 = 2;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+
+console.log(`Resultado da Soma entre ${num1} e ${num2} é ${soma}`);
+console.log(`Resultado da subtração entre ${num1} e ${num2} é ${subtracao}`);
+console.log(`Resultado da multiplicação entre ${num1} e ${num2} é ${multiplicacao}`);
+console.log(`Resultado da Divisão entre ${num1} e ${num2} é ${divisao}`);
+
